b805ba4d3f1508193ceea7501d2c4e8e89a5fbf2
QINQIN <qinqin@MacBook-Pro-de-QINQIN.local>
2026-01-07 17:24:42 +0100

    fix production settings

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -9,16 +9,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
+    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
-STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    *MIDDLEWARE,
-]
+] + MIDDLEWARE
